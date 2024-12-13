c15169b7392c8d224a88ac5fbf3f887b64b33342
Harshini Borugadda <harshiniborugadda.1104@gmail.com>
2024-12-13 05:55:16 +0530

    Added navbar and removed unused imports

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import dash_bootstrap_components as dbc
-from dash import html, dcc, Dash, Output, Input, State
+from dash import Dash, Output, Input, State
 from models import model, tfidf
 
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
@@ -17,6 +17,9 @@ def calculate(n_clicks, value):
     return prediction * 100, "danger" if prediction > 0.66 else "warning" if prediction > 0.33 else "success" 
 
 app.layout = [
+    dbc.Navbar([
+        dbc.NavbarBrand("SpamNotSpam"),
+    ], dark=True, color="dark", class_name="py-2 px-4"),
     dbc.Stack([
         dbc.Input(placeholder="Enter your text", id="input-text"),
         dbc.Button("Calculate", id="submit-button"),
